4d208f99cfd0ca07df1585b04e11669d1dd4cf6b
b-613t <youngsrc@gmail.com>
2026-02-12 04:46:43 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ko-KR",
+    baseUrl: "b-613t.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
